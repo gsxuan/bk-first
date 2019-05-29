8fc35ef38706b6de0dadf44f7996af1bc64c8b36
龚胜轩 <gsx@weiyian.com>
2019-05-29 22:26:45 +0800

    js

--- home_application/views.py
+++ home_application/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
 # 装饰器引入 from blueapps.account.decorators import login_exempt
@@ -21,8 +21,14 @@ def render_json(res_dict):
     return HttpResponse(json.dumps(res_dict), content_type='application/json')
 
 # 输入值提交请求
-def sayhello(request):
+def say_hello(request):
     data = request.POST.get('input', None)
     data = 'Congratulations!' if data == 'Hello Blueking' else 'Try input Hello Blueking'
     res = {'data': data}
+    pass
     return render_json(res)
+
+
+@csrf_exempt
+def ajaxview(request):
+    pass
